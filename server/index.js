3b17ac2a74a54d03c01cbd7db79dcd9fb44c4dff
Floella Otudeko <ella@Floellas-Air.fritz.box>
2023-03-19 17:36:34 +0000

    index.js updated

--- server/index.js
+++ server/index.js
@@ -8,3 +8,19 @@ const cors = require('cors');
 // Parse incoming requests
 app.use(express.json());
 app.use(cors());
+
+app.get('/messages', (req, res) => {
+  // Code to retrieve messages
+});
+
+app.post('/messages', (req, res) => {
+  // Code to add a new message
+});
+
+// Server instance and listen for incoming requests
+const server = http.createServer(app);
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
