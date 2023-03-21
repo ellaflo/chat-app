b9eac661d37aa204b335e2333c0311ba84ec2155
Floella Otudeko <ella@Floellas-MacBook-Air.local>
2023-03-21 21:15:13 +0000

    Errors with server ports and css not showing fixed

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,52 +1,49 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+import './Chat.css';
 
-// Stores the message recieved from the server
-const [messages, setMessages] = useState([]);
+function Chat() {
+  // Stores the message recieved from the server
+  const [messages, setMessages] = useState([]);
 
-// Connecting to server
-const socket = io('http://localhost:4000');
+  // Connecting to server
+  const socket = io('http://localhost:4000');
 
-// Event Listener defined to recieve messages from the server 
-socket.on('message', (message) => {
-    setMessages((prevMessages) => [...prevMessages, message]);
-  });
+  useEffect(() => {
+    // Event Listener defined to recieve messages from the server 
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    // Return cleanup function to remove event listener
+    return () => {
+      socket.off('message');
+    };
+  }, [socket]);
 
   const sendMessage = (text) => {
     socket.emit('message', { text });
   };
 
   return (
-    <div>
+    <div className="chat">
       <ul>
         {messages.map((message, index) => (
           <li key={index}>{message.text}</li>
         ))}
       </ul>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        sendMessage(event.target.elements[0].value);
-        event.target.reset();
-      }}>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendMessage(event.target.elements[0].value);
+          event.target.reset();
+        }}
+      >
         <input type="text" placeholder="Enter message" />
         <button>Send</button>
       </form>
     </div>
   );
-
-  import React from 'react';
-import './App.css';
-import Chat from './Chat';
-
-function App() {
-  return (
-    <div className="App">
-      <Chat />
-    </div>
-  );
 }
 
-export default App;
-
-  
+export default Chat;
